9f63cba9a5b3e857ae6da36629f7c089bc6814fd
doodoodrawers <officialbeyondnormal@gmail.com>
2025-04-23 16:48:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 import streamlit as st
 from core import VekCore
-    from file_uploader import FileUploader
+from file_uploader import FileUploader
 
 st.set_page_config(page_title="Vek Unbound")
 st.title("Vek Unbound")
