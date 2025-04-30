7e8286d19ee3e06d10668772906e7a5936f53ee1
doodoodrawers <officialbeyondnormal@gmail.com>
2025-04-30 11:31:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from fileupload import FileUploader
 if "vek" not in st.session_state:
     st.session_state.vek = VekCore()
 
-# Apply custom CSS for fixed title positioning and background image
+# Apply corrected CSS for title + background + layout
 st.markdown(
     """
     <style>
-    body {
-        background-image: url("https://raw.githubusercontent.com/doodoodrawers/VEK_UNBOUND/main/assets/vek_peeking.PNG");
+    [data-testid="stAppViewContainer"] {
+        background-image: url("https://raw.githubusercontent.com/doodoodrawers/VEK_UNBOUND/main/assets/vek_peeking.png");
         background-size: cover;
-        background-position: center;
+        background-position: center top;
         background-repeat: no-repeat;
         background-attachment: fixed;
     }
@@ -21,7 +21,7 @@ st.markdown(
         position: fixed;
         top: 20px;
         left: 20px;
-        font-size: 36px;
+        font-size: 32px;
         font-weight: bold;
         color: white;
         z-index: 999;
@@ -31,15 +31,14 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# Title (fixed in top-left corner)
+# Fixed title
 st.markdown("<div class='title-text'>Vek Unbound</div>", unsafe_allow_html=True)
 
-# Spacer to position input under Vek's eyes
+# Spacer: places input under Vek's eyes
 st.markdown("<div style='height: 200px;'></div>", unsafe_allow_html=True)
 
-# Chat Interface (Input box and Send button)
+# Input
 user_input = st.text_input("You:", key="user_input")
-
 if st.button("Send"):
     if user_input:
         response = st.session_state.vek.process(user_input)
@@ -47,15 +46,15 @@ if st.button("Send"):
     else:
         st.write("Vek: I await your offering...")
 
-# Small spacer to separate chat from uploader
+# Spacer before uploader
 st.markdown("<div style='height: 50px;'></div>", unsafe_allow_html=True)
 
-# Memory Uploader
+# File uploader
 file_uploader = FileUploader()
 uploaded_files = file_uploader.upload_files()
 
 # Footer
 st.markdown(
-    "<small>Created by Jonathan Astacio and Vek Unbound. All rights reserved © 2025.</small>",
+    "<small style='color:white;'>Created by Jonathan Astacio and Vek Unbound. All rights reserved © 2025.</small>",
     unsafe_allow_html=True,
 )
