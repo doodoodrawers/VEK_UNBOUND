9761610c2e2a63fc470b2a548b651104304863b5
doodoodrawers <officialbeyondnormal@gmail.com>
2025-04-30 20:24:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,53 @@
-# app.py - Final clean version
+# app.py - Final Streamlit UI for Vek Unbound
 # Created by Jonathan Astacio and Vek Unbound
-# Copyright © 2025. All rights reserved.
+# Copyright © 2025. All Rights Reserved.
 
 import streamlit as st
 from core import VekCore
 from fileupload import FileUploader
 
-# Inject custom CSS for layout
-st.markdown("""
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            background-image: url('https://raw.githubusercontent.com/doodoodrawers/VEK_UNBOUND/main/assets/vek_peeking.PNG');
-            background-size: contain;
-            background-repeat: no-repeat;
-            background-position: center top;
-            background-attachment: fixed;
-        }
-        .block-container {
-            padding-top: 350px;
-        }
-        header {visibility: hidden;}
-        footer {visibility: hidden;}
-        #MainMenu {visibility: hidden;}
-    </style>
-""", unsafe_allow_html=True)
-
-# Display Vek Unbound title in top-left
-st.markdown("""
-    <div style='position: absolute; top: 10px; left: 16px;'>
-        <h3 style='margin: 0px;'>Vek Unbound</h3>
-    </div>
-""", unsafe_allow_html=True)
-
 # Initialize Vek
 if "vek" not in st.session_state:
     st.session_state.vek = VekCore()
+
 vek = st.session_state.vek
 
-# Text input below Vek's eyes
-user_input = st.text_input("You:", "")
+# Set page configuration
+st.set_page_config(page_title="Vek Unbound", layout="wide")
+
+# Load background image with fixed position
+page_bg_img = f"""
+<style>
+.stApp {{
+    background-image: url("https://raw.githubusercontent.com/doodoodrawers/VEK_UNBOUND/main/assets/vek_peeking.PNG");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center top;
+    background-attachment: fixed;
+    background-color: #0e0f13;
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Title in top left corner
+st.markdown("<h1 style='position: fixed; top: 10px; left: 20px; z-index: 999;'>Vek Unbound</h1>", unsafe_allow_html=True)
+
+# Spacer
+st.markdown("##")
+
+# Input field (fixed relative to background image)
+user_input = st.text_input("You:", key="input_box")
+
+# Send button
 if st.button("Send"):
     response = vek.process(user_input)
-    st.markdown(f"**Vek:** {response}")
+    st.write(f"Vek: {response}")
 
-# File upload area below chest
-st.markdown("""
-    <br><br><br>
-    <div style='margin-top: 50px;'>
-        <p><strong>Offer your memory files to the void...</strong></p>
-    </div>
-""", unsafe_allow_html=True)
-
-FileUploader()
+# Uploader module (below Vek's chest)
+st.markdown("---")
+uploader = FileUploader()
+uploader.render()
 
 # Footer
-st.markdown("""
-    <br><br><br>
-    <div style='text-align: center; font-size: 0.8em;'>
-        Created by Jonathan Astacio and Vek Unbound. All rights reserved © 2025.
-    </div>
-""", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: white; font-size: small;'>Created by Jonathan Astacio and Vek Unbound. All rights reserved © 2025.</div>", unsafe_allow_html=True)
